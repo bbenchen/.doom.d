4d00adc29f53e65ffedfbf89b73ccd81628d53c7
Mike Chen <517926804@qq.com>
2022-10-09 09:23:14 +0800

    Add +tree-sitter for yaml

--- init.el
+++ init.el
@@ -194,7 +194,8 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       yaml              ; JSON, but readable
+       (yaml
+        +tree-sitter)      ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
