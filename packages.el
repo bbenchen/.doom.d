e9c4630ecc2f77b314c2b683924e8c46e0188e17
Mike Chen <517926804@qq.com>
2023-01-05 12:20:49 +0800

    Update lsp-bridge

--- packages.el
+++ packages.el
@@ -104,7 +104,7 @@
   :recipe (:host github
            :repo "manateelazycat/lsp-bridge"
            :files ("*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources"))
-  :pin "0aec1c0a0514a2305a201e5f9465c25a2c850325")
+  :pin "7dfeeb640d14697755e2ac7997af0ec6c413197f")
 
 (when (modulep! :lang go)
   (package! go-fill-struct :pin "a613d0b378473eef39e8fd5724abe790aea84321")
