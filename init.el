200d1f759a191ed4bd5f9f21d7255bb90d94d153
Mike Chen <517926804@qq.com>
2023-03-14 14:05:53 +0800

    Enable tree-sitter for lua

--- init.el
+++ init.el
@@ -161,7 +161,8 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       (lua
+        +tree-sitter)    ; one-based indices? one-based indices
        (markdown +grip)  ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
