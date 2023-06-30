48ad5d42d680768967b88d2df60f77ab8cd04461
Mike Chen <517926804@qq.com>
2023-06-30 11:21:37 +0800

    Update init module

--- init.el
+++ init.el
@@ -107,7 +107,7 @@
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       (:if IS-MAC pdf)  ; pdf enhancements
+       ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
@@ -162,7 +162,7 @@
        ;;ledger            ; be audit you can be
        (lua
         +tree-sitter)    ; one-based indices? one-based indices
-       (markdown +grip)  ; writing docs for people to ignore
+       markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
