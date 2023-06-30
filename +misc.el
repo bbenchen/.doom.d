fbc9a90a51746de8e209b3df3ac35fcb7e15dfb8
Mike Chen <517926804@qq.com>
2023-06-30 14:04:29 +0800

    Delete chatgpt

--- +misc.el
+++ +misc.el
@@ -282,6 +282,3 @@
     (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
     (add-to-list 'TeX-view-program-list '("eaf" eaf-pdf-synctex-forward-view))
     (add-to-list 'TeX-view-program-selection '(output-pdf "eaf"))))
-
-(use-package! chatgpt
-  :load-path (lambda () (list (expand-file-name "site-lisp/chatgpt" doom-user-dir))))
