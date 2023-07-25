d9bdfb524ad86fdb24a22dc3416f805fcdd7c888
Mike Chen <517926804@qq.com>
2023-07-25 16:51:34 +0800

    Enable ligatures module

--- init.el
+++ init.el
@@ -39,7 +39,8 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       (ligatures
+        +extra)          ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        (modeline +light) ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
@@ -167,10 +168,10 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
+        +crypt
         +dragndrop
         +pandoc
-        +pretty
-        +crypt)          ; organize your plain life in plain text
+        +pretty)         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
