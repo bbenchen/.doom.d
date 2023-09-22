f63b2b0c14a1446621b9b188480180efc873d3e9
Mike Chen <517926804@qq.com>
2023-09-22 10:01:34 +0800

    Update lsp-bridge

--- packages.el
+++ packages.el
@@ -111,7 +111,7 @@
            :repo "cxb811201/lsp-bridge"
            :files ("*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
            :build (:not compile))
-  :pin "7730d0d004ba8923282b71cee1ca7595a8395bf2")
+  :pin "7c5e5935de1ef78e3955657cd2eeda7f9e0504f7")
 
 (when (modulep! :lang go)
   (package! go-impl :pin "1eebba6ccd02d11a5a82ad4540a8d562797bc3b3")
