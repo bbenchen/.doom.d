cd47024a811d498bdeb6968d916560f7ce31c9d1
Mike Chen <517926804@qq.com>
2023-09-28 17:03:16 +0800

    Remove the +pretty flag from the org module.

--- init.el
+++ init.el
@@ -172,8 +172,7 @@
        (org
         +crypt
         +dragndrop
-        +pandoc
-        +pretty)         ; organize your plain life in plain text
+        +pandoc)         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
