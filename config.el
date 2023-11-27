5f075d635101698fa1da074029c60495842d518e
Mike Chen <517926804@qq.com>
2023-11-27 09:16:27 +0800

    Update user-full-name

--- config.el
+++ config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Mike Chen"
+(setq user-full-name "Ben Chan"
       user-mail-address "517926804@qq.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
