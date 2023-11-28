f467bdfb1f10ca58d9b22bb57e18c2709d070c5a
Mike Chen <517926804@qq.com>
2023-11-28 14:13:16 +0800

    Unpin snippets

--- packages.el
+++ packages.el
@@ -49,6 +49,8 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
+(unpin! (:editor snippets))
+
 (package! tide :disable t)
 (package! tree-sitter-indent :disable t)
 
