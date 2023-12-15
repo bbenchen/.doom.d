6e4c6f5014c0dd331921568620fb1d15661013c6
Mike Chen <517926804@qq.com>
2023-12-15 15:54:47 +0800

    Update sbt-mode configuration

--- +prog.el
+++ +prog.el
@@ -225,6 +225,12 @@
 
 ;; sbt
 (after! sbt-mode
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
   (setq sbt:program-options '("-Dsbt.supershell=false")))
 
