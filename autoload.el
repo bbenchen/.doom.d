8c074b49848c2ac20d9f87f715c8c3485d395237
Ben Chen <517926804@qq.com>
2024-02-01 10:22:44 +0800

    Update alpha-background to 60

--- autoload.el
+++ autoload.el
@@ -79,8 +79,8 @@
 If FRAME is nil, it defaults to the selected frame."
   (interactive)
   (pcase (frame-parameter frame 'alpha-background)
-    (50 (modify-frame-parameters frame '((alpha-background . 100))))
-    (_ (modify-frame-parameters frame '((alpha-background . 50))))))
+    (60 (modify-frame-parameters frame '((alpha-background . 100))))
+    (_ (modify-frame-parameters frame '((alpha-background . 60))))))
 
 ;; Network Proxy
 ;;;###autoload
