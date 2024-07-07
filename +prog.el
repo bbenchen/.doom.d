8b2a832b4347a970613b098a25bb993e329ca8b8
Ben Chen <517926804@qq.com>
2024-07-08 00:26:57 +0800

    Update lsp-bridge configuration
    use basedpyright as the LSP server for Python

--- +prog.el
+++ +prog.el
@@ -376,8 +376,8 @@
         lsp-bridge-diagnostic-tooltip-border-width 2
         lsp-bridge-signature-show-function 'lsp-bridge-signature-show-with-frame
         lsp-bridge-signature-show-with-frame-position "point"
-        lsp-bridge-python-lsp-server "ruff"
-        lsp-bridge-python-multi-lsp-server "pyright_ruff"
+        lsp-bridge-python-lsp-server "basedpyright"
+        lsp-bridge-python-multi-lsp-server "basedpyright_ruff"
         acm-backend-yas-match-by-trigger-keyword t
         acm-backend-search-file-words-enable-fuzzy-match t
         acm-enable-capf t
