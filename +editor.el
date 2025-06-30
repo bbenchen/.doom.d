12a7cb1d5098bbd7035925e1a8c90731cff0c39f
Ben Chen <517926804@qq.com>
2025-06-30 20:37:49 +0800

    Remove wraplish

--- +editor.el
+++ +editor.el
@@ -74,13 +74,6 @@
   (add-hook! 'doom-after-init-hook :append
     (visual-replace-global-mode 1)))
 
-;; wraplish
-(use-package! wraplish
-  :config
-  (setq wraplish-add-space-after-chinese-punctuation t)
-
-  (add-hook! '(prog-mode-hook text-mode-hook conf-mode-hook) #'wraplish-mode))
-
 ;; deno-bridge-jieba
 (use-package! deno-bridge-jieba
   :load-path (lambda () (list (expand-file-name "site-lisp/deno-bridge-jieba" doom-user-dir)))
