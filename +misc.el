3a8b078f50aa86fae514ab0687023e8a9e32265b
Ben Chen <517926804@qq.com>
2025-11-24 18:26:17 +0800

    Improve aidermacs configuration

--- +misc.el
+++ +misc.el
@@ -433,10 +433,10 @@
   :commands aidermacs-run aidermacs-transient-menu
   :config
   (setq aidermacs-auto-commits nil
-        aidermacs-default-model "openrouter/deepseek/deepseek-r1-0528:free"
+        aidermacs-default-model "openrouter/deepseek/deepseek-chat-v3.1"
         aidermacs-default-chat-mode 'architect
         aidermacs-architect-model "openrouter/moonshotai/kimi-k2-0905"
-        aidermacs-editor-model "openrouter/deepseek/deepseek-r1-0528"
+        aidermacs-editor-model "openrouter/deepseek/deepseek-chat-v3.1"
         aidermacs-weak-model "openrouter/deepseek/deepseek-chat-v3-0324"
         aidermacs-extra-args '("--no-check-update" "--chat-language" "Chinese")
         aidermacs-watch-files t
