2a035899f96ca67e979e6c6c3bd9685a5194542b
Ben Chen <517926804@qq.com>
2026-04-07 11:17:32 +0800

    Upgrade packages

--- packages.el
+++ packages.el
@@ -54,7 +54,7 @@
 (package! project :pin "041a69d062a5442e2b0fee125c3228225f894ce1" :built-in (> emacs-major-version 30))
 (package! which-key :pin "38d4308d1143b61e4004b6e7a940686784e51500" :built-in 'prefer)
 
-(package! vterm :pin "a01a2894a1c1e81a39527835a9169e35b7ec5dec")
+(package! vterm :pin "54c29d14bca05bdd8ae60cda01715d727831e3f9")
 
 (package! posframe :pin "3a80911b2f45ce6926196930bb7d5cc662c7b3c8")
 
@@ -80,7 +80,7 @@
   :recipe (:host github :repo "lorniu/gt.el")
   :pin "f9febd8583ea482f72139e02f440f3972502f5a2")
 (package! immersive-translate :pin "1d00d558363985fa988fc40cd5093bfc6926d83e")
-(package! llm :pin "ed10ac00712a4d09d35c4b65dca8d2e851ddcdd6")
+(package! llm :pin "7ac8aab062e1ddd7f99a6ef6b0ee417f0ef0a7a8")
 (package! insert-translated-name
   :recipe (:host github :repo "manateelazycat/insert-translated-name" :files ("*.el" "*.py"))
   :pin "a2bc301f6e99a6f965f7612c5666546e3d1a8fe3")
@@ -115,7 +115,7 @@
 (package! aidermacs
   :recipe (:host github :repo "MatthewZMD/aidermacs")
   :pin "6d0c41d1cfd24821fb32933edf8c0c2a9bb8c847")
-(package! gptel :recipe (:nonrecursive t) :pin "d6c920d4c09c732868881d0cdad59c98439d398c")
+(package! gptel :recipe (:nonrecursive t) :pin "cd4375a53525a399f64adfc99c791b35c890d152")
 (package! gptel-quick
   :recipe (:host github :repo "karthink/gptel-quick")
   :pin "018ff2be8f860a1e8fe3966eec418ad635620c38")
